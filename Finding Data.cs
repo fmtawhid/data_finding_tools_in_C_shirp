c3823075d6a461d5758b0d19f2df810eae706fa8
Tawhid Islam <79523960+fmtawhid@users.noreply.github.com>
2022-12-13 21:16:14 +0600

    Program Upload

---
+++ Finding Data.cs
@@ -0,0 +1,82 @@
+using System;
+
+public class HelloWorld
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine ("Hello Mono World");
+        for (int xox = 0; xox < 5; xox++)
+        {
+        int x =Convert.ToInt32(Console.ReadLine());
+        switch (x)
+        {
+        case 1:
+            Console.WriteLine("Name: Tawhid ul islam");
+            Console.WriteLine("Class: Diploma");
+            Console.WriteLine("Dream: Entrepreneur");
+            break;
+        case 2:
+            Console.WriteLine("Name: Nasim");
+            Console.WriteLine("Age: 37");
+            Console.WriteLine("Class: Web Developer");
+            Console.WriteLine("Dream: programmer");
+            break;
+        case 3:
+            Console.WriteLine("Name: Asik ");
+            Console.WriteLine("Age: 20");
+            Console.WriteLine("Class: 12");
+            Console.WriteLine("Dream: Teacher");
+            break;
+        case 4:
+            Console.WriteLine("Name: Tawhid ul islam");
+            Console.WriteLine("Age: 18");
+            Console.WriteLine("Class: Diploma");
+            Console.WriteLine("Dream: Entrepreneur");
+            break;
+        case 5:
+            Console.WriteLine("Name: Tawhid ul islam");
+            Console.WriteLine("Age: 18");
+            Console.WriteLine("Class: Diploma");
+            Console.WriteLine("Dream: Entrepreneur");
+            break;
+        case 6:
+            Console.WriteLine("Name: Tawhid ul islam");
+            Console.WriteLine("Age: 18");
+            Console.WriteLine("Class: Diploma");
+            Console.WriteLine("Dream: Entrepreneur");
+            break;
+        case 7:
+            Console.WriteLine("Name: Tawhid ul islam");
+            Console.WriteLine("Age: 18");
+            Console.WriteLine("Class: Diploma");
+            Console.WriteLine("Dream: Entrepreneur");
+            break;
+        case 8:
+            Console.WriteLine("Name: Tawhid ul islam");
+            Console.WriteLine("Age: 18");
+            Console.WriteLine("Class: Diploma");
+            Console.WriteLine("Dream: Entrepreneur");
+            break;
+        case 9:
+            Console.WriteLine("Name: Tawhid ul islam");
+            Console.WriteLine("Age: 18");
+            Console.WriteLine("Class: Diploma");
+            Console.WriteLine("Dream: Entrepreneur");
+            break;
+        case 10:
+            Console.WriteLine("Name: Tawhid ul islam");
+            Console.WriteLine("Age: 18");
+            Console.WriteLine("Class: Diploma");
+            Console.WriteLine("Dream: Entrepreneur");
+            break;
+        case 11:
+            Console.WriteLine("Name: Tawhid ul islam");
+            Console.WriteLine("Age: 18");
+            Console.WriteLine("Class: Diploma");
+            Console.WriteLine("Dream: Entrepreneur");
+            break;
+        }
+        }
+        
+    }
+}
